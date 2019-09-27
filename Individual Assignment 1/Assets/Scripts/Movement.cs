0e55bf7b34ae21c93cfc6120c8f48f4ee59da92e
Kevin <kevin.lounsbury@uoit.net>
2019-09-27 12:11:40 -0400

    Movement Tweaked

--- Individual Assignment 1/Assets/Scripts/Movement.cs
+++ Individual Assignment 1/Assets/Scripts/Movement.cs
@@ -16,21 +16,25 @@ public class Movement : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.W))
         {
-            transform.position = new Vector3(0, 1, 0);
+            transform.position = new Vector3(transform.position.x, 
+                transform.position.y + 1, transform.position.z);
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
-            transform.position = new Vector3(-1, 0, 0);
+            transform.position = new Vector3(transform.position.x - 1, 
+                transform.position.y, transform.position.z);
 
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            transform.position = new Vector3(0, -1, 0);
+            transform.position = new Vector3(transform.position.x, 
+                transform.position.y - 1, transform.position.z);
 
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.position = new Vector3(1,0,0);
+            transform.position = new Vector3(transform.position.x+1, 
+                transform.position.y, transform.position.z);
 
         }
     }
